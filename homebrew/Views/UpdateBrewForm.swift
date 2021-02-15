a15512903a1e4b06dd27e3fd461fc9fc5e3bab33
Lucas Desouza <lucasfds2777@gmail.com>
2021-02-14 22:34:07 -0600

    adding comment field

--- homebrew/Views/UpdateBrewForm.swift
+++ homebrew/Views/UpdateBrewForm.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct UpdateBrewForm: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @State var name = ""
+    @State var comment = ""
+    @State var showDescriptionEditor = false
     @State var gravity = ""
     @State var date = Date()
     
@@ -19,6 +21,11 @@ struct UpdateBrewForm: View {
         Form {
             TextField("Name", text: $name)
                 .padding()
+            Button(action: {
+                    showDescriptionEditor = true
+            }) {
+                comment == "" ? Text("Description").foregroundColor(.gray) : Text(comment).foregroundColor(.primary)
+            }.padding()
             TextField("Original gravity", text: $gravity)
                 .keyboardType(.decimalPad)
                 .padding()
@@ -29,8 +36,19 @@ struct UpdateBrewForm: View {
                 .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
         }.onAppear() {
             self.name = brew.name!
+            self.comment = brew.comment ?? ""
             self.gravity = "\(brew.originalGravity)"
             self.date = brew.startDate!
+        }.sheet(isPresented: $showDescriptionEditor) {
+            VStack {
+                TextEditor(text: self.$comment).padding()
+                Spacer()
+                Button(action: {
+                    self.showDescriptionEditor = false
+                }){
+                  Text("Save")
+                }.padding()
+            } 
         }.navigationTitle("Update brew")
     }
     
@@ -38,6 +56,7 @@ struct UpdateBrewForm: View {
         withAnimation {
             brew.startDate = date
             brew.name = name
+            brew.comment = comment
             brew.originalGravity = Double(gravity) ?? 1.000
             do {
                 try brew.managedObjectContext!.save()
