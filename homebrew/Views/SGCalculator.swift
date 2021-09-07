afcb58c55677362e1f336ed01063bc696f10206d
Lucas Desouza <lucasfds2777@gmail.com>
2021-09-07 18:18:46 -0500

    adding picker for sugar source

--- homebrew/Views/SGCalculator.swift
+++ homebrew/Views/SGCalculator.swift
@@ -7,8 +7,30 @@
 
 import SwiftUI
 
+
+
+struct SugarType: Hashable {
+    var name: String
+    var points: Double
+}
+
+let sugars = [
+    SugarType(name: "Honey", points: 0.035),
+    SugarType(name: "Sugar", points: 0.042)
+]
+
 struct SGCalculator: View {
-    @State var pointsPerPound = ""
+    var sugars = [
+        SugarType(name: "Dry Malt Extract", points: 0.044),
+        SugarType(name: "Honey", points: 0.035),
+        SugarType(name: "Liquid Malt Extract", points: 0.036),
+        SugarType(name: "Molasses", points: 0.036),
+        SugarType(name: "Honey", points: 0.046),
+        SugarType(name: "Wheat Malt", points: 0.029),
+        SugarType(name: "Wheat Torrefied", points: 0.027),
+        SugarType(name: "2-Row Pale Malt", points: 0.027),
+    ]
+    @State var sugar: Int? = nil
     @State var quantity = ""
     @State var batchSize = ""
     @State var result: Double = 0.0
@@ -17,9 +39,12 @@ struct SGCalculator: View {
             Text("\(result)")
                 .font(.largeTitle)
                 .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
-            TextField("Fermentable Sugar", text: $pointsPerPound)
-                .keyboardType(.decimalPad)
-                .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+            Picker(selection: $sugar, label: Text("Sugar source")) {
+                Text("None").tag(nil as Int?)
+                ForEach(0 ..< sugars.count) {
+                    Text(self.sugars[$0].name).tag($0 as Int?)
+                }
+            }.frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
             TextField("Sugar Quantity (pounds)", text: $quantity)
                 .keyboardType(.decimalPad)
                 .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
@@ -32,12 +57,13 @@ struct SGCalculator: View {
             .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
         }.navigationTitle("ABV Calculator")
     }
-    
+
     private func calculate() {
-        let pointsPerPoundConverted = Double(pointsPerPound) ?? 0.00
-        let quantityConverted = Double(quantity) ?? 0.00
-        let batchSizeConverted = Double(batchSize) ?? 0.00
-        result = quantityConverted * (pointsPerPoundConverted / batchSizeConverted)
+        if let sugar = self.sugar {
+            let quantityConverted = Double(quantity) ?? 0.00
+            let batchSizeConverted = Double(batchSize) ?? 0.00
+            result = quantityConverted * (sugars[sugar].points / batchSizeConverted)
+        }
     }
 }
 
