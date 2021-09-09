4bbac87eef1299252b827e182dc45e47cda735eb
Lucas Desouza <lucasfds2777@gmail.com>
2021-09-09 16:54:31 -0500

    adding in secondary record and moving edit button

---
+++ homebrew/Views/NewSecondaryRecord.swift
@@ -0,0 +1,44 @@
+//
+//  NewSecondaryRecord.swift
+//  iBru
+//
+//  Created by Lucas Desouza on 9/9/21.
+//
+
+import SwiftUI
+
+struct NewSecondaryRecord: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @State var gravity = ""
+    @State var date = Date()
+    
+    var brew: Brew
+    
+    var body: some View {
+        Form {
+            TextField("Final gravity", text: $gravity)
+                .keyboardType(.decimalPad)
+                .padding()
+            DatePicker("Bottle date", selection: $date)
+                .datePickerStyle(GraphicalDatePickerStyle())
+            Button("Finish", action: addSecondary)
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+        }.navigationTitle("Record secondary")
+    }
+    
+    private func addSecondary() {
+        withAnimation {
+            let newItem = Secondary(context: brew.managedObjectContext!)
+            newItem.date = date
+            newItem.gravity = Double(gravity) ?? 1.000
+            brew.secondary = newItem
+            do {
+                try brew.managedObjectContext!.save()
+                presentationMode.wrappedValue.dismiss()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+}
