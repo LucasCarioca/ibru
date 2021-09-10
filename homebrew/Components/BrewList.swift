6a91ac022eca25ab294af133f749fda1ec3d88f7
Lucas Desouza <lucasfds2777@gmail.com>
2021-09-10 15:05:23 -0500

    reformat

--- homebrew/Components/BrewList.swift
+++ homebrew/Components/BrewList.swift
@@ -8,13 +8,19 @@
 import SwiftUI
 
 struct BrewList: View {
+    @Environment(\.managedObjectContext) private var viewContext
     var brews: FetchedResults<Brew>
     var body: some View {
         List {
             ForEach(brews) { brew in
                 BrewTile(brew: brew)
-            }
+            }.onDelete(perform: { offsets in
+                withAnimation {
+                    deleteBrew(offsets: offsets, brews: brews, context: viewContext)
+                }
+            })
         }.listStyle(PlainListStyle())
         .modifier(NewBrewButtonModifier())
     }
 }
+
