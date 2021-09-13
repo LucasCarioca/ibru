b25a47c54042c5acef34741a55c9e6e56e7ca082
Lucas Desouza <lucasfds2777@gmail.com>
2021-09-12 21:10:04 -0500

    adding 1 year progress bar

--- homebrew/Components/CollectionTile.swift
+++ homebrew/Components/CollectionTile.swift
@@ -6,49 +6,62 @@
 //
 
 import SwiftUI
+import QuickComponents
 
 struct CollectionTile: View {
     var brew: Brew
     var onChange: () -> Void
     var body: some View {
-        VStack{
-            HStack {
-                VStack {
-                    HStack {
-                        Text(brew.name ?? "missing name")
-                            .font(.title2)
-                            .fontWeight(.heavy)
-                            
-                        Spacer()
-                    }.padding()
-                    getBottleCount()
-                    InfoLabel(label: "Age", value: getTimeText())
-                }
-                Spacer()
-                VStack {
-                    Button(action: {print("adding"); addOrRemoveBottle(amount: 1)}) {
-                        Image(systemName: "plus.circle.fill")
-                            .imageScale(.large)
-                    }.contentShape(Rectangle())
+        HStack {
+            VStack {
+                HStack {
+                    Text(brew.name ?? "missing name")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        
                     Spacer()
-                    Button(action: {print("removing"); addOrRemoveBottle(amount: -1)}) {
-                        Image(systemName: "minus.circle.fill")
-                            .imageScale(.large)
-                    }.contentShape(Rectangle())
                 }.padding()
+                getBottleCount()
+                InfoLabel(label: "Age", value: getTimeText())
+                getProgressBar()
             }
-            Divider()
+            Spacer()
+            VStack {
+                Button(action: {print("adding"); addOrRemoveBottle(amount: 1)}) {
+                    Image(systemName: "plus.circle.fill")
+                        .imageScale(.large)
+                }
+                    .buttonStyle(BorderlessButtonStyle())
+                    .contentShape(Rectangle())
+                Spacer()
+                Button(action: {print("removing"); addOrRemoveBottle(amount: -1)}) {
+                    Image(systemName: "minus.circle.fill")
+                        .imageScale(.large)
+                }
+                    .buttonStyle(BorderlessButtonStyle())
+                    .contentShape(Rectangle())
+            }.padding()
         }
     }
     
     func getTimeText() -> String {
-        var brewAge = getAge(of: brew)
+        var brewAge = getAge(of: brew, fromBottlingDate: true)
         var label = "Days"
         if brewAge >= 30 {
             brewAge = brewAge / 30
             label = "Months"
         }
         return "\(String(format: "%.1f", brewAge)) \(label)"
+    }
+    
+    func getProgressBar() -> BarView {
+        var age = getAge(of: brew, fromBottlingDate: true)
+        if age < 30 {
+            age = 30
+        } else if age >= 365 {
+            age = 365
+        }
+        return BarView(value: CGFloat(age), max: 365, showLabel: nil, color: .accentColor)
     }
     
     func getBottleCount() -> AnyView {
