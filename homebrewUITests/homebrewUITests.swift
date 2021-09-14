4c5518a8f60b5fb8ffb810ec688934e5b492be62
Lucas Desouza <lucasfds2777@gmail.com>
2021-09-14 09:27:04 -0500

    adding new screenshots and navigation ui test

--- homebrewUITests/homebrewUITests.swift
+++ homebrewUITests/homebrewUITests.swift
@@ -7,33 +7,56 @@
 
 import XCTest
 
-class homebrewUITests: XCTestCase {
-
+class baseTests: XCTestCase {
+    let app: XCUIApplication = XCUIApplication()
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+        app.launchArguments = ["-test-data"]
         app.launch()
+        continueAfterFailure = false
+    }
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    override func tearDownWithError() throws {}
+
+    func testBasicNavigation() throws {
+        
+        let dashboardNavigationBar = app.navigationBars["Dashboard"]
+        XCTAssert(dashboardNavigationBar.staticTexts["Dashboard"].exists)
+        dashboardNavigationBar.buttons["Menu"].tap()
+        XCTAssert(app.navigationBars["Menu"].staticTexts["Menu"].exists)
+
+
+        let tablesQuery = app.tables
+        tablesQuery.buttons["Collection"].tap()
+        XCTAssert(app.navigationBars["Collection"].staticTexts["Collection"].exists)
+        tablesQuery.buttons["IPA Beer, Bottle Count: , 50, Age: , 2.5 Months"].tap()
+        let beerIpaNavigationBar = app.navigationBars["IPA Beer"]
+        XCTAssert(beerIpaNavigationBar.staticTexts["IPA Beer"].exists)
+        XCTAssert(app.staticTexts["Primary Fermentation"].exists)
+        XCTAssert(app.staticTexts["Bottled"].exists)
+        beerIpaNavigationBar.buttons["Collection"].tap()
+        app.navigationBars["Collection"].buttons["Menu"].tap()
+        
+        
+        tablesQuery.cells["Brew List"].otherElements.containing(.button, identifier:"Brew List").element.tap()
+        let brewsNavigationBar = app.navigationBars["Brews"]
+        XCTAssert(brewsNavigationBar.staticTexts["Brews"].exists)
+        brewsNavigationBar.buttons["Menu"].tap()
+        
+    
+        tablesQuery/*@START_MENU_TOKEN@*/.buttons["Calculators"]/*[[".cells[\"Calculators\"].buttons[\"Calculators\"]",".buttons[\"Calculators\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCTAssert(app.navigationBars["Calculators"].staticTexts["Calculators"].exists)
+        tablesQuery.cells["Alcohol by volume"].otherElements.containing(.button, identifier:"Alcohol by volume").element.tap()
+        let abvCalculatorNavigationBar = app.navigationBars["ABV Calculator"]
+        XCTAssert(abvCalculatorNavigationBar.staticTexts["ABV Calculator"].exists)
+        abvCalculatorNavigationBar.buttons["Calculators"].tap()
+        tablesQuery/*@START_MENU_TOKEN@*/.buttons["Gravity Estimator"]/*[[".cells[\"Gravity Estimator\"].buttons[\"Gravity Estimator\"]",".buttons[\"Gravity Estimator\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCTAssert(app.navigationBars["Gravity Estimator"].staticTexts["Gravity Estimator"].exists)
+        
     }
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
             }
