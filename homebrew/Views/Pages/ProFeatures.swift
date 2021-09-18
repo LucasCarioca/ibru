5c924dc4c19fa7ae9790b3e2d5f74947d48947e5
Lucas Desouza <lucasfds2777@gmail.com>
2021-09-17 22:53:01 -0500

    adding storemanager

--- homebrew/Views/Pages/ProFeatures.swift
+++ homebrew/Views/Pages/ProFeatures.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import QuickComponents
 
 struct ProFeatures: View {
+    @EnvironmentObject var storeManager: StoreManager
     var body: some View {
         VStack {
-            Text("Coming Soon")
-        }.navigationTitle("Upgrade to Pro")
+            Text("Upgrade to Pro").Heading(align: .center, size: .H1)
+            Text("Enable all advanced features like the Collection to manage previous batches and the Dashboard to monitor your brews at its different stages.").Paragraph(align: .center, size: .MD)
+            Spacer()
+            Button(action: initiatePurchase) {
+                Text("Upgrade Now")
+            }
+                .buttonStyle(PrimaryButton(variant: .contained))
+                .frame(width: 150, height: 75)
+        }
+    }
+    
+    func initiatePurchase() {
+        let product = storeManager.myProducts[0]
+        storeManager.purchaseProduct(product: product)
     }
 }
 
