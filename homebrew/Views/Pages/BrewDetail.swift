02586281208eadb63a9290ab037e83f34c07e41f
Lucas Desouza <lucasfds2777@gmail.com>
2021-11-19 12:31:29 -0600

    feat: separate the readings page to make it more visible

--- homebrew/Views/Pages/BrewDetail.swift
+++ homebrew/Views/Pages/BrewDetail.swift
@@ -45,48 +45,19 @@ struct BrewDetail: View {
                 showSecondaryInfo().padding(.vertical)
             }
             showBottlingInfo().padding(.vertical)
-            Spacer()
-            HStack {
-                Text("Readings")
-                    .font(.title2)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                Spacer()
-            }.padding()
-            ZStack {
-                List {
-                    ForEach(getReadingList(), id: \.self) { reading in
-                        NavigationLink(destination: UpdateReadingForm(reading: reading).onDisappear(perform: refresh)){
-                            GravityReading(date: reading.date ?? Date(), gravity: reading.gravity, originalGravity: brew.originalGravity)
-                        }
-                    }.onDelete(perform: deleteItems)
-                    Spacer().padding(.vertical, 50)
-                }
-                showButtons()
+            NavigationLink(destination: Readings(brew: brew).onDisappear(perform: refresh)) {
+                HStack {
+                    Image(systemName: "square.and.pencil").padding(.leading)
+                    Text("Readings")
+                        .fontWeight(.bold)
+                        .padding(.trailing)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }.padding(.vertical)
             }
+            Spacer()
         }
         .navigationTitle(brew.name ?? "Missing name")
         .id(refreshID)
-    }
-    
-    private func getReadingList() -> [Reading] {
-        let set = brew.readings! as? Set<Reading> ?? []
-        return set.sorted {
-            $0.date! < $1.date!
-        }
-    }
-    
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            let readingList = getReadingList()
-            offsets.map { readingList[$0] }.forEach(brew.managedObjectContext!.delete)
-            do {
-                try brew.managedObjectContext!.save()
-                refresh()
-            } catch {
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
     }
     
     private func showAge(fromBottlingDate: Bool = false, fromSecondary: Bool = false) -> AnyView {
@@ -97,31 +68,6 @@ struct BrewDetail: View {
             brewAge = brewAge/30
         }
         return AnyView(Text("\(String(format: "%.1f", brewAge)) \(label)"))
-    }
-    
-    private func showButtons() -> AnyView {
-        if let _ = brew.bottles {
-            return AnyView(EmptyView())
-        }
-        return AnyView(VStack {
-            Spacer()
-            HStack {
-                Spacer()
-                NavigationLink(destination: NewReadingForm(brew: brew).onDisappear(perform: refresh)){
-                    Image("testtube").colorInvert()
-                        .font(.system(.largeTitle))
-                        .frame(width: 67, height: 67)
-                        .background(Color.accentColor)
-                        .cornerRadius(50)
-                        .padding(.vertical)
-                        .padding(.trailing, 12)
-                        .shadow(color: Color.black.opacity(0.3),
-                                radius: 3,
-                                x: 3,
-                                y: 3)
-                }
-            }
-        })
     }
     
     private func showBottlingInfo() -> AnyView {
