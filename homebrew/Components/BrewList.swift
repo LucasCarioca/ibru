ce90a86d0ff126466506871ac982567625d67386
Lucas Desouza <lucasfds2777@gmail.com>
2021-11-22 15:18:19 -0600

    fix: auto pop out bug on macos

--- homebrew/Components/BrewList.swift
+++ homebrew/Components/BrewList.swift
@@ -17,13 +17,19 @@ struct BrewList: View {
         self.onEmpty = AnyView(onEmpty)
     }
 
+    @State var refreshId = UUID()
+
     var body: some View {
         if brews.count <= 0 {
             onEmpty
         } else {
             List {
                 ForEach(brews) { brew in
-                    BrewTile(brew: brew)
+                    NavigationLink(destination: BrewDetail(brew: brew).onDisappear() {
+                        refreshId = UUID()
+                    }) {
+                        BrewTile(brew: brew)
+                    }
                 }.onDelete(perform: { offsets in
                     withAnimation {
                         deleteBrew(offsets: offsets, brews: brews, context: viewContext)
