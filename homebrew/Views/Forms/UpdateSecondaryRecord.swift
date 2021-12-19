35fd256155342582ddacd933a8cf07d454f659ac
Lucas Desouza <lucasfds2777@gmail.com>
2021-12-19 14:01:25 -0600

    feat: adding update secondary

---
+++ homebrew/Views/Forms/UpdateSecondaryRecord.swift
@@ -0,0 +1,47 @@
+//
+//  NewSecondaryRecord.swift
+//  iBru
+//
+//  Created by Lucas Desouza on 9/9/21.
+//
+
+import SwiftUI
+
+struct UpdateSecondaryRecord: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @State var gravity = ""
+    @State var date = Date()
+
+    var secondary: Secondary
+
+    var body: some View {
+        Form {
+            TextField("Current gravity", text: $gravity)
+                    .keyboardType(.decimalPad)
+                    .padding()
+            DatePicker("Bottle date", selection: $date)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+            Button("Finish", action: updateSecondary)
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+        }
+                .onAppear() {
+                    self.gravity = String(secondary.gravity)
+                    self.date = secondary.date ?? Date()
+                }
+                .navigationTitle("Secondary")
+    }
+
+    private func updateSecondary() {
+        withAnimation {
+            secondary.gravity = Double(gravity) ?? 1.000
+            secondary.date = date
+            do {
+                try secondary.managedObjectContext!.save()
+                presentationMode.wrappedValue.dismiss()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
+}
