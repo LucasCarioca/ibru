841ea7adb92b4f03d95dd0f38d61a3f990094f26
Lucas Desouza <lucasfds2777@gmail.com>
2021-12-31 13:11:43 -0600

    cleanup review functions

--- homebrew/Common/RequestReview.swift
+++ homebrew/Common/RequestReview.swift
@@ -6,15 +6,29 @@ import Foundation
 import StoreKit
 
 func requestReview(in scene: UIWindowScene) {
-    var count = UserDefaults.standard.integer(forKey: "processCompletedCountKey")
+    let (version, build) = getVersion()
+    var (count, lastVersionPromptedForReview) = getSessionCount()
     count += 1
-    UserDefaults.standard.set(count, forKey: "processCompletedCountKey")
-    let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
-    let build = Bundle.main.object(forInfoDictionaryKey: kCFBundleVersionKey as String) as! String
+    setSessionCount(count)
     let currentVersion = "\(version) (\(build))"
-    let lastVersionPromptedForReview = UserDefaults.standard.string(forKey: "lastVersionPromptedForReviewKey")
     if count >= 4 && currentVersion != lastVersionPromptedForReview {
         SKStoreReviewController.requestReview(in: scene)
         UserDefaults.standard.set(currentVersion, forKey: "lastVersionPromptedForReviewKey")
     }
+}
+
+func getVersion() -> (version: String, build: String) {
+    let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+    let build = Bundle.main.object(forInfoDictionaryKey: kCFBundleVersionKey as String) as! String
+    return (version, build)
+}
+
+func getSessionCount() -> (count: Int, version: String) {
+    let version = UserDefaults.standard.string(forKey: "lastVersionPromptedForReviewKey")
+    let count = UserDefaults.standard.integer(forKey: "processCompletedCountKey")
+    return (count, version ?? "")
+}
+
+func setSessionCount(_ count: Int) {
+    UserDefaults.standard.set(count, forKey: "processCompletedCountKey")
 }
